aa5d74cde3bde760b5aae12dfa75ea1ddc50a7a0
孙桢波 <sunzb20@mails.tsinghua.edu.cn>
2023-04-18 23:38:12 +0800

    add scripts

--- examples/sc23/test_end_to_end_16.sh
+++ examples/sc23/test_end_to_end_16.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 
-# 1.3B model
+# 2.6B model
 GLOBAL_BATCH_SIZE=512
 NUM_MICRO_BATCH=64
 
-NUM_LAYERS=24
-HIDDEN_SIZE=2048
+NUM_LAYERS=32
+HIDDEN_SIZE=2560
 NUM_HEADS=32
-RESULT_DIR="end_to_end_result_8"
+RESULT_DIR="end_to_end_result_16"
 MASTER_ADDR="nico1"
 
 LOCAL_BATCH_SIZE=$(expr $GLOBAL_BATCH_SIZE / $NUM_MICRO_BATCH)
 
-num=8
+num=16
 
 if [ ! -d $RESULT_DIR ]; then
   mkdir $RESULT_DIR
@@ -33,7 +33,7 @@ do
         GLOBAL_BATCH_SIZE=$GLOBAL_BATCH_SIZE MICRO_BATCH_SIZE=$MICRO_BATCH_SIZE \
         NUM_LAYERS=$NUM_LAYERS HIDDEN_SIZE=$HIDDEN_SIZE NUM_HEADS=$NUM_HEADS \
         MASTER_ADDR=$MASTER_ADDR \
-        srun -n $num -p Big -K -w nico1 sh examples/pretrain_gpt.sh | tee ${RESULT_DIR}/gpt_${TP}_${DP}_${PP}.txt
+        srun -n 16 --ntasks-per-node 8 -p Big -K -w nico1,nico2 sh examples/pretrain_gpt.sh | tee ${RESULT_DIR}/gpt_${TP}_${DP}_${PP}.txt
 
         # openmpi
         # mpirun --allow-run-as-root -x TP=$TP -x DP=$PP \
---
+++ examples/sc23/test_intra_stage_8.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+# 1.3B model
+GLOBAL_BATCH_SIZE=512
+NUM_MICRO_BATCH=64
+
+NUM_LAYERS=4
+HIDDEN_SIZE=4096
+NUM_HEADS=32
+RESULT_DIR="intra_stage_result_4"
+MASTER_ADDR="nico1"
+
+LOCAL_BATCH_SIZE=$(expr $GLOBAL_BATCH_SIZE / $NUM_MICRO_BATCH)
+
+num=4
+
+if [ ! -d $RESULT_DIR ]; then
+  mkdir $RESULT_DIR
+fi
+
+TP=1
+PP=1
+while [ $TP -le $num ]
+do
+    DP=$(expr $num / $TP)
+    MICRO_BATCH_SIZE=$(expr $LOCAL_BATCH_SIZE / $DP)
+    echo "test DP: $DP, TP: $TP, PP: $PP"
+
+    TP=$TP PP=$PP \
+    GLOBAL_BATCH_SIZE=$GLOBAL_BATCH_SIZE MICRO_BATCH_SIZE=$MICRO_BATCH_SIZE \
+    NUM_LAYERS=$NUM_LAYERS HIDDEN_SIZE=$HIDDEN_SIZE NUM_HEADS=$NUM_HEADS \
+    MASTER_ADDR=$MASTER_ADDR \
+    srun -n $num -p Big -K -w nico1 sh examples/pretrain_gpt.sh | tee ${RESULT_DIR}/gpt_${TP}_${DP}_${PP}.txt
+
+    # openmpi
+    # mpirun --allow-run-as-root -x TP=$TP -x DP=$PP \
+    # -x GLOBAL_BATCH_SIZE=$GLOBAL_BATCH_SIZE -x MICRO_BATCH_SIZE=$MICRO_BATCH_SIZE \
+    # -x NUM_LAYERS=$NUM_LAYERS -x HIDDEN_SIZE=$HIDDEN_SIZE -x NUM_HEADS=$NUM_HEADS \
+    # -x LD_LIBRARY_PATH -x MASTER_ADDR=$MASTER_ADDR \
+    # sh examples/pretrain_gpt.sh | tee ${RESULT_DIR}/gpt_${TP}_${DP}_${PP}.txt
+
+    TP=$(expr $TP \* 2)
+done
